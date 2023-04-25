360aeb022ddf2b5294b7e50d79ac473910ff59da
ngocnhanne <nguyenngocnhan2104@gmail.com>
2023-04-25 17:47:53 +0700

    change

--- backend/src/main/java/com/medisure/medisure_as/repository/MedicalProcessRepository.java
+++
@@ -1,8 +0,0 @@
-package com.medisure.medisure_as.repository;
-
-import com.medisure.medisure_as.entity.Doctors;
-import com.medisure.medisure_as.entity.MedicalProcess;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicalProcessRepository extends JpaRepository<MedicalProcess,Long> {
-}
--- backend/src/main/java/com/medisure/medisure_as/repository/SpecialistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.medisure.medisure_as.repository;
-
-import com.medisure.medisure_as.entity.Schedule;
-import com.medisure.medisure_as.entity.Specialist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpecialistRepository extends JpaRepository<Specialist,Long> {
-}
